e42dea59847f3dca903527204d1ab8b7d70e2524
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-19 21:15:47 +0200

    floating window for git push

--- .scripts/git/kiity-prompt-float.sh
+++ .scripts/git/kiity-prompt-float.sh
@@ -1,18 +1,7 @@
 
 #!/bin/bash
 
-PROMPT_MESSAGE="$1"
-TEMP_FILE=$(mktemp)
-
 # Run kitty with a prompt for user input
-hyprctl dispatch exec "[float] kitty --title 'Git Commit Message' --execute bash -c 'read -p \"$PROMPT_MESSAGE\" msg; echo \"\$msg\" > $TEMP_FILE'"
+hyprctl dispatch exec "[float] kitty $HOME/.scripts/git/gitOSpush.sh"
 
-sleep 0.5  # Adjust if needed for window to appear
 
-# Read the user input from the temporary file
-commit_message=$(cat "$TEMP_FILE")
-
-# Clean up the temporary file
-rm "$TEMP_FILE"
-
-echo "$commit_message"
