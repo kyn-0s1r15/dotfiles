a5bf0f90e2f8f00a9b3b0418879710a516fa051b
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-19 21:32:38 +0200

    fixed path declerations

--- install.sh
+++ install.sh
@@ -1,28 +1,34 @@
 
 #!/bin/bash
-cp -r $HOME/LICENSE
-cp -r $HOME/README.md
-cp -r $HOME/install.sh
-cp -r $HOME/.config/gtk-3.0
-cp -r $HOME/.config/hypr
-cp -r $HOME/.config/kitty
-cp -r $HOME/.config/powerlevel10k
-cp -r $HOME/.config/rofi
-cp -r $HOME/.config/wal
-cp -r $HOME/.config/waybar
-cp -r $HOME/.icons
-cp -r $HOME/.local/share/applications
-cp -r $HOME/.oh-my-zsh
-cp -r $HOME/.scripts
-cp -r $HOME/DEV
-cp -r $HOME/.p10k.zsh
-cp -r $HOME/.zshrc
-cp -r $HOME/.config/nixos/configuration.nix
+
+ cp -r $HOME/dotfiles/LICENSE $HOME/
+ cp -r $HOME/dotfiles/README.md $HOME/
+ cp -r $HOME/dotfiles/install.sh $HOME/
+ cp -r $HOME/dotfiles/.config/gtk-3.0 $HOME/
+ cp -r $HOME/dotfiles/.config/hypr $HOME/
+ cp -r $HOME/dotfiles/.config/kitty $HOME/
+ cp -r $HOME/dotfiles/.config/powerlevel10k $HOME/
+ cp -r $HOME/dotfiles/.config/rofi $HOME/
+ cp -r $HOME/dotfiles/.config/wal $HOME/
+ cp -r $HOME/dotfiles/.config/waybar $HOME/
+ cp -r $HOME/dotfiles/.icons $HOME/
+ cp -r $HOME/dotfiles/.local/share/applications $HOME/
+ cp -r $HOME/dotfiles/.oh-my-zsh $HOME/
+ cp -r $HOME/dotfiles/.scripts $HOME/
+ cp -r $HOME/dotfiles/DEV $HOME/
+ cp -r $HOME/dotfiles/.p10k.zsh $HOME/
+ cp -r $HOME/dotfiles/.zshrc $HOME/
+ cp -r $HOME/dotfiles/.config/nixos/configuration.nix $HOME/
 
 sleep 1
 
-sudo cp $HOME/.config/nixos/configuration.nix /etc/nixos/
+ sudo cp $HOME/.config/nixos/configuration.nix /etc/nixos/
+ sudo cp $HOME/dotfiles/.config/nixos/configuration.nix /etc/nixos/
 
 sleep 1
 
-sudo nixos-rebuild boot
+ sudo nixos-rebuild boot
+
+sleep 1
+
+ echo "time to reboot :)"
