fcb886b9c93af94b97f1c0029c369744523014aa
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-19 20:33:13 +0200

    Add directories from ~/.scripts/git/gitdirs

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 
 #!/bin/bash
+<<<<<<< HEAD
 cp -r ~/dotfiles/* ~/
 
 cp $HOME/.config/nixos/configuration.nix /etc/nixos/
+=======
+sudo cp -r $HOME/dotfiles/* $HOME/ &&
+
+sleep 1
+
+sudo cp $HOME/.config/nixos/configuration.nix /etc/nixos/
+>>>>>>> ff2f17a (Add directories from ~/.scripts/git/gitdirs)
 
 sleep 1
 
