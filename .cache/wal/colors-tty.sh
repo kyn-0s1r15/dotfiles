706437ce64849f295e2b3f942d962a8f669796f8
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-24 20:42:14 +0200

    i

---
+++ .cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0080C15
+                 \e]P1A86154
+                 \e]P2D7976B
+                 \e]P35F7389
+                 \e]P48B7B87
+                 \e]P56C8EA5
+                 \e]P69F989F
+                 \e]P7e3d5cc
+                 \e]P89e958e
+                 \e]P9A86154
+                 \e]PAD7976B
+                 \e]PB5F7389
+                 \e]PC8B7B87
+                 \e]PD6C8EA5
+                 \e]PE9F989F
+                 \e]PFe3d5cc
+                 \ec'
