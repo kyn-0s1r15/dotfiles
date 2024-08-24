834da7f4f5a318dc5a86ee6a192c65f67162a975
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-24 20:42:19 +0200

    i

---
+++ .cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#080C15";
+static const char* selfgcolor   = "#e3d5cc";
+static const char* normbgcolor  = "#D7976B";
+static const char* normfgcolor  = "#e3d5cc";
+static const char* urgbgcolor   = "#A86154";
+static const char* urgfgcolor   = "#e3d5cc";
