d708dcc931157bf04d3fd69a263b9b810ce845d3
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-24 20:42:17 +0200

    i

---
+++ .cache/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e3d5cc", "#080C15" },
+	[SchemeSel] = { "#e3d5cc", "#A86154" },
+	[SchemeOut] = { "#e3d5cc", "#9F989F" },
+};
