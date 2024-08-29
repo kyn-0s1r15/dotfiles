a327badab519596e5237c75c054ec332731603af
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-08-29 18:21:15 +0200

    cleaned up routine

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -8,15 +8,10 @@ sleep 1
  cp -r $HOME/dotfiles/LICENSE $HOME/
  cp -r $HOME/dotfiles/README.md $HOME/
  cp -r $HOME/dotfiles/install.sh $HOME/
- cp -r $HOME/dotfiles/.config/gtk-3.0 $HOME/
- cp -r $HOME/dotfiles/.config/hypr $HOME/
- cp -r $HOME/dotfiles/.config/kitty $HOME/
- cp -r $HOME/dotfiles/.config/powerlevel10k $HOME/
- cp -r $HOME/dotfiles/.config/rofi $HOME/
- cp -r $HOME/dotfiles/.config/wal $HOME/
- cp -r $HOME/dotfiles/.config/waybar $HOME/
+ cp -r $HOME/dotfiles/.config $HOME/
+ cp -r $HOME/dotfiles/.cache $HOME/
  cp -r $HOME/dotfiles/.icons $HOME/
- cp -r $HOME/dotfiles/.local/share/applications $HOME/
+ cp -r $HOME/dotfiles/.local $HOME/
  cp -r $HOME/dotfiles/.oh-my-zsh $HOME/
  cp -r $HOME/dotfiles/.scripts $HOME/
  cp -r $HOME/dotfiles/DEV $HOME/
@@ -26,7 +21,6 @@ sleep 1
 
 sleep 1
 
- sudo cp $HOME/.config/nixos/configuration.nix /etc/nixos/
  sudo cp $HOME/dotfiles/.config/nixos/configuration.nix /etc/nixos/
 
 sleep 1
