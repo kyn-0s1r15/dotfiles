6c76285601150d8141404209d2cbe8f5e4b6c12b
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-09-06 15:08:10 +0200

    autocommit

--- .cache/wal/colors-wal-dmenu.h
+++ .cache/wal/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e3d5cc", "#080C15" },
-	[SchemeSel] = { "#e3d5cc", "#A86154" },
-	[SchemeOut] = { "#e3d5cc", "#9F989F" },
+	[SchemeNorm] = { "#e9cbc0", "#121948" },
+	[SchemeSel] = { "#e9cbc0", "#324DA0" },
+	[SchemeOut] = { "#e9cbc0", "#AEA5BA" },
 };
