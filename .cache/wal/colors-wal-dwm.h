2d1b21012835fa2581f61976971f6ec1aacb5497
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-09-06 15:08:12 +0200

    autocommit

--- .cache/wal/colors-wal-dwm.h
+++ .cache/wal/colors-wal-dwm.h
@@ -1,14 +1,14 @@
-static const char norm_fg[] = "#e3d5cc";
-static const char norm_bg[] = "#080C15";
-static const char norm_border[] = "#9e958e";
+static const char norm_fg[] = "#e9cbc0";
+static const char norm_bg[] = "#121948";
+static const char norm_border[] = "#a38e86";
 
-static const char sel_fg[] = "#e3d5cc";
-static const char sel_bg[] = "#D7976B";
-static const char sel_border[] = "#e3d5cc";
+static const char sel_fg[] = "#e9cbc0";
+static const char sel_bg[] = "#5867A2";
+static const char sel_border[] = "#e9cbc0";
 
-static const char urg_fg[] = "#e3d5cc";
-static const char urg_bg[] = "#A86154";
-static const char urg_border[] = "#A86154";
+static const char urg_fg[] = "#e9cbc0";
+static const char urg_bg[] = "#324DA0";
+static const char urg_border[] = "#324DA0";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
