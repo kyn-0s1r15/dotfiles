36010c7fcd2b8de4999daec4738addf841c5cc9d
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-10-25 14:19:35 +0200

    autogen

--- .cache/wal/colors-tty.sh
+++ .cache/wal/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P0121948
-                 \e]P1324DA0
-                 \e]P25867A2
-                 \e]P39C748C
-                 \e]P47785A9
-                 \e]P56790DB
-                 \e]P6AEA5BA
-                 \e]P7e9cbc0
-                 \e]P8a38e86
-                 \e]P9324DA0
-                 \e]PA5867A2
-                 \e]PB9C748C
-                 \e]PC7785A9
-                 \e]PD6790DB
-                 \e]PEAEA5BA
-                 \e]PFe9cbc0
+    printf '%b' '\e]P00c0f18
+                 \e]P1234E64
+                 \e]P2614C68
+                 \e]P3AB4A41
+                 \e]P4A14E5F
+                 \e]P5DC5F5B
+                 \e]P6F0915A
+                 \e]P79baabe
+                 \e]P86c7685
+                 \e]P9234E64
+                 \e]PA614C68
+                 \e]PBAB4A41
+                 \e]PCA14E5F
+                 \e]PDDC5F5B
+                 \e]PEF0915A
+                 \e]PF9baabe
                  \ec'
