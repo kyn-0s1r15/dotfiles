c327cfd380aadcb191c082c503d8a6a57e862c47
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-10-25 14:19:38 +0200

    autogen

--- .cache/wal/colors-wal-dmenu.h
+++ .cache/wal/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e9cbc0", "#121948" },
-	[SchemeSel] = { "#e9cbc0", "#324DA0" },
-	[SchemeOut] = { "#e9cbc0", "#AEA5BA" },
+	[SchemeNorm] = { "#9baabe", "#0c0f18" },
+	[SchemeSel] = { "#9baabe", "#234E64" },
+	[SchemeOut] = { "#9baabe", "#F0915A" },
 };
