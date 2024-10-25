6d8bb22ab6dca6d06f86191594cb42946cd4710a
kyn-0s1r15 <mrkynastonblake@gmail.com>
2024-10-25 14:19:42 +0200

    autogen

--- .cache/wal/colors-wal-st.h
+++ .cache/wal/colors-wal-st.h
@@ -1,29 +1,29 @@
 const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#121948", /* black   */
-  [1] = "#324DA0", /* red     */
-  [2] = "#5867A2", /* green   */
-  [3] = "#9C748C", /* yellow  */
-  [4] = "#7785A9", /* blue    */
-  [5] = "#6790DB", /* magenta */
-  [6] = "#AEA5BA", /* cyan    */
-  [7] = "#e9cbc0", /* white   */
+  [0] = "#0c0f18", /* black   */
+  [1] = "#234E64", /* red     */
+  [2] = "#614C68", /* green   */
+  [3] = "#AB4A41", /* yellow  */
+  [4] = "#A14E5F", /* blue    */
+  [5] = "#DC5F5B", /* magenta */
+  [6] = "#F0915A", /* cyan    */
+  [7] = "#9baabe", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#a38e86",  /* black   */
-  [9]  = "#324DA0",  /* red     */
-  [10] = "#5867A2", /* green   */
-  [11] = "#9C748C", /* yellow  */
-  [12] = "#7785A9", /* blue    */
-  [13] = "#6790DB", /* magenta */
-  [14] = "#AEA5BA", /* cyan    */
-  [15] = "#e9cbc0", /* white   */
+  [8]  = "#6c7685",  /* black   */
+  [9]  = "#234E64",  /* red     */
+  [10] = "#614C68", /* green   */
+  [11] = "#AB4A41", /* yellow  */
+  [12] = "#A14E5F", /* blue    */
+  [13] = "#DC5F5B", /* magenta */
+  [14] = "#F0915A", /* cyan    */
+  [15] = "#9baabe", /* white   */
 
   /* special colors */
-  [256] = "#121948", /* background */
-  [257] = "#e9cbc0", /* foreground */
-  [258] = "#e9cbc0",     /* cursor */
+  [256] = "#0c0f18", /* background */
+  [257] = "#9baabe", /* foreground */
+  [258] = "#9baabe",     /* cursor */
 };
 
 /* Default colors (colorname index)
